f2a37642dbdcc147439a317c1a54c945dc751102
Jeff Williams <88175879+orphandeity@users.noreply.github.com>
2023-12-24 17:05:48 -0800

    adds createUser query function

--- db/queries/users.js
+++ db/queries/users.js
@@ -1,9 +1,21 @@
 const knex = require('../connection')
 
+function createUser({ username, password }) {
+  return knex('users')
+    .insert({
+      username,
+      password,
+      avatar: `https://robohash.org/${username}?set=set4&size=128x128`,
+      role: 'user'
+    })
+    .returning('*')
+}
+
 function getUserByUsername(username) {
   return knex('users').where({ username }).first()
 }
 
 module.exports = {
+  createUser,
   getUserByUsername
 }
